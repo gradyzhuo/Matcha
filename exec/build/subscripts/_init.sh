1b27a1195f5d516c5be8aa35af53fb91bb78a926
Grady Zhuo <gradyzhuo@gmail.com>
2017-03-21 19:57:13 +0800

    @divide the commands: basic/build/install.

---
+++ exec/build/subscripts/_init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# _init.sh
+#
+# Matcha 1.0, Created by gradyzhuo, enoch_lee.
+# Copyright © 2017, Matcha Inc. All rights reserved.
+#
+
+will_exec "init"
+# 如果不存在 rvm 這個指令
+if [[ $(exists -c rvm) == 1 ]]; then
+	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+	\curl -sSL https://get.rvm.io | bash
+
+	rvm install 2.3.1
+	rvm use 2.3.1 --default
+fi
+
+# 如果不存在 sigh 這個指令
+if [[ $(exists -c fastlane) == 1 ]]; then
+	sudo gem install fastlane -NV
+fi
+
+did_exec "init"
---
+++ exec/build/subscripts/clone_app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# git.sh
+#
+# Matcha 1.0, Created by gradyzhuo, enoch_lee.
+# Copyright © 2017, Matcha Inc. All rights reserved.
+#
+
+will_exec "git"
+
+phase_print "Cloning app"
+
+clone -r "${GIT_APP}" -b "${GIT_BRANCH_APP}" -t "${APP_PATH}" -tag $GIT_TAG_APP || \
+terminate "$GIT_CLOME_FAIL_CODE 'Git clone failed."
+
+did_exec "git"
