b467dfdfd75afc3028d7513a8419ff897d6d8d9f
Grady Zhuo <gradyzhuo@gmail.com>
2017-03-23 11:23:09 +0800

    @refactor.

--- exec/build/subscripts/setup_codesign.sh
+++ exec/build/subscripts/setup_codesign.sh
@@ -10,22 +10,24 @@ will_exec "setup_codesign"
 
 configure_signing_identity
 
+PROVISIONING_PROFILE="profile"
+
 #===== Provision Profile =====
 
 if [[ $NEED_DOWNLOAD_PROVISION_PROFILE == 0 ]]; then
   phase_print "Downloading provision profile"
-  profile download -fetch "$PROVISIONING_PROFILE_NAME" -option "$EXPORT_OPTION" -id "$APPLE_ID" -passwd "$APPLE_ID_PASSWORD" -app_id "$APP_ID" -team "$TEAM_NAME" -output "$PROVISIONING_FOLDER/profile"
-  profile install -profile "$PROVISIONING_FOLDER/profile"
+  profile download -fetch "$PROVISIONING_PROFILE_NAME" -option "$EXPORT_OPTION" -id "$APPLE_ID" -passwd "$APPLE_ID_PASSWORD" -app_id "$APP_ID" -team "$TEAM_NAME" -output "$PROVISIONING_FOLDER/$PROVISIONING_PROFILE"
+  profile install -profile "$PROVISIONING_FOLDER/$PROVISIONING_PROFILE"
 fi
 
 if [[ $AUTOMATICALLY_MANAGE_SIGIN != 0 && "$UUID" == "" ]]; then
   phase_print "Fetching UUID"
-  UUID=$(profile showUUID -profile "$PROVISIONING_FOLDER/profile")
+  UUID=$(profile showUUID -profile "$PROVISIONING_FOLDER/$PROVISIONING_PROFILE")
   print -c green "using provision profile: $PROVISIONING_PROFILE($UUID)"
 fi
 
 if [[ $NEED_DOWNLOAD_PROVISION_PROFILE == 0 ]]; then
-  clean provision_profile
+  profile clean -profile "$PROVISIONING_FOLDER/$PROVISIONING_PROFILE"
 fi
 
 did_exec "setup_codesign"
